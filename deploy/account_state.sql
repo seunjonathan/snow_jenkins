bf937b7f2e6645708f21fd730d7f1527eed68f88
Seun Jonathanm <seund@yahoo.com>
2025-01-31 11:34:49 -0700

    p

--- deploy/account_state.sql
+++ deploy/account_state.sql
@@ -3,3 +3,8 @@ Create database if not exists SLF_ADMIN_ALL COMMENT = 'DBA ADT';
 Create schema if not exists SLF_ADMIN_ALL.SLF_ADMIN_SCHEMA;
 
 Create schema if not exists SLF_ADMIN_ALL.MONITORING;
+
+
+Create tag if not exists SLF_ADMIN_ALL.MONITORING.cost_center;
+
+Create tag if not exists SLF_ADMIN_ALL.MONITORING.environment;
