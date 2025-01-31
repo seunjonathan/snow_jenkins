aa8985223d8ed14d44a9d6a42b20eaa1a7d08664
Seun Jonathanm <seund@yahoo.com>
2025-01-31 11:40:28 -0700

    p

--- deploy/account_state.sql
+++ deploy/account_state.sql
@@ -1,4 +1,7 @@
+
+// DATABASES and SCHEMAS
 Create database if not exists SLF_ADMIN_ALL COMMENT = 'DBA ADT';
+drop schema if exists SLF_ADMIN_ALL.PUBLIC;
 
 Create schema if not exists SLF_ADMIN_ALL.SLF_ADMIN_SCHEMA;
 
@@ -8,3 +11,7 @@ Create schema if not exists SLF_ADMIN_ALL.MONITORING;
 Create tag if not exists SLF_ADMIN_ALL.MONITORING.cost_center;
 
 Create tag if not exists SLF_ADMIN_ALL.MONITORING.environment;
+
+
+// WHs
+create warehouse if not exists XS_WH_DEV comment ='wokload';
